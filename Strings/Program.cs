b93038b176544b782b25c151175cf0479532251b
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-31 01:35:47 +0300

    4.1

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -8,18 +8,21 @@ namespace Strings
     {
         public static string SplitString(string usrInput)
         {
+            //What about multiple white spaces, and other white spaces char?
             var split = usrInput.Split(' ');
             return "The number of words is: " + split.Length;
         }
 
         static string ReverseString(string usrInput)
         {
+            //This is not the first time you have called string.Split( ''). This is code duplicatoin.
             var reversedString = string.Join(" ", usrInput.Split(' ').Reverse());
             return "The reversed string is: " + reversedString;
         }
 
         static string SortedArray(string usrInput)
         {
+            //This is not the first time you have called string.Split( ''). This is code duplicatoin.
             string[] sortedString = usrInput.Split(' ');
             Array.Sort(sortedString);
             return "The sorted string is: " + string.Join(" ", sortedString);
